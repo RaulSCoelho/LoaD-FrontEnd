91de0150517a1088b9ef8d3e997636f59f75baf4
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2022-04-11 02:43:08 -0300

    scrollvideosOk

---
+++ src/components/BackOrForthButton.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const BackOrForthButton = styled.div`
+    width: 72px;
+    height: 72px;
+    background-color: rgba(0, 0, 0, 0.75);
+    border-radius: 50%;
+    border: 2px solid white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`
--- src/components/CurrentVideo.js
+++ src/components/CurrentVideo.js
@@ -13,18 +13,23 @@ function CurrentVideo() {
     return (
         <ThemeProvider theme={Themes.flexRow}>
             <Flex>
+                {/* Botão para voltar pro video anterior */}
                 <div className="toggleVideo">
                     <Back size="1.8em" color="white" />
                 </div>
                 <div className="currentVideoDiv">
+                    {/* Vídeo atual */}
                     <Video className="currentVideo" url={`${localStorage.getItem('currentVideo')}`} title={VideosInfo.titles[0][1]} width={854} />
+                    {/* Módulo da aula */}
                     <ThemeProvider theme={Themes.smallTitle}>
                         <Title>{`${localStorage.getItem('currentVideoModule')}`}</Title>
                     </ThemeProvider>
+                    {/* Título da aula */}
                     <ThemeProvider theme={Themes.bigTitle}>
                         <Title>{`${localStorage.getItem('currentVideoLesson')}`}</Title>
                     </ThemeProvider>
                 </div>
+                {/* Botão para avançar para o próximo video */}
                 <div className="toggleVideo">
                     <Forward size="1.8em" color="white" />
                 </div>
