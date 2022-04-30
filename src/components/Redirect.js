be704ee3a1f7397ace8f367667c919e08a209175
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2022-04-30 19:00:00 -0300

    redirect

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -1,7 +1,5 @@
-let url = "https://lifeofadream.vercel.app"
-
 export const Redirect = (path) => {
     window.location.href = window.location.hostname === "localhost" ? `http://localhost:3000${path}`
         : window.location.hostname === "192.168.0.87" ? `http://192.168.0.87:3000${path}`
-            : url + path
+            : `https://lifeofadream.vercel.app${path}`
 }
